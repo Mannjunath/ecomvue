7a948e858deeec9deda946c71ab25883f8afb0fb
mannju <manjunath.rs@unilogcorp.com>
2018-04-12 19:21:06 +0530

    categories page content

---
+++ ecombase/src/Pages/Categories.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+      <div class="col-md-6">
+          <h5>{{title}}</h5>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "Categories"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
