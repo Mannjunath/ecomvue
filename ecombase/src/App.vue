6e890f5e027801c40e82b51379cf135fb6ab8789
Manjunath GK <manjunath.gk@unilogcorp.com>
2018-04-13 19:47:42 +0530

    footer content added

--- ecombase/src/App.vue
+++ ecombase/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <main>
-    <app-header></app-header>
-    <mainnav></mainnav>
+    <app-header />
+    <mainnav />
     <router-view/>
+    <app-footer />
   </main>
 </template>
 
 <script>
 import Header from './Layouts/header'
 import MainNav from './Layouts/main_nav'
+import Footer from './Layouts/footer'
 import appstyle from './scss/appstyle.scss'
 
 export default {
   components:{
     'app-header': Header,
-    'mainnav' : MainNav
+    'mainnav' : MainNav,
+    'app-footer': Footer,
   },
   name: 'App'
 }
---
+++ ecombase/src/Layouts/footer.vue
@@ -0,0 +1,68 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4" v-for="footerList in footerLinks" :key="footerList">
+                    <h5>{{ footerList.linkHead }}</h5>
+                    <ul>
+                        <li v-for="footerSubList in footerList.sublink" :key="footerSubList">
+                            {{ footerSubList.sublinkName }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+export default {
+  data() {
+      return{
+        footerLinks: [{
+            linkHead: 'Link 1',
+            "sublink":[
+                {"sublinkName": "sub Link 1","url": ''},
+                {"sublinkName": "sub Link 2","url": ''}, 
+                {"sublinkName": "sub Link 3","url": ''}
+            ]},
+            {
+            linkHead: 'Link 2',
+            "sublink":[
+                    {"sublinkName": "sub Link 1","url": ''},
+                    {"sublinkName": "sub Link 2","url": ''}, 
+                    {"sublinkName": "sub Link 3","url": ''}
+                ]
+            },
+            {
+            linkHead: 'Link 3',
+            "sublink":[
+                    {"sublinkName": "sub Link 1","url": ''},
+                    {"sublinkName": "sub Link 2","url": ''}, 
+                    {"sublinkName": "sub Link 3","url": ''},
+                    {"sublinkName": "sub Link 4","url": ''}
+                ]
+            },
+        ]
+      }
+  }
+}
+</script>
+
+<style>
+footer{
+    background: #404040;
+    min-height: 100px;
+    padding: 10px;
+    color: #FFF;
+}
+ul{
+    padding:0;
+    margin:0;
+}
+li{
+    list-style-type: none;
+    
+}
+</style>
