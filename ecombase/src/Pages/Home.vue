e3d87d892d07e79d4067e71ab2deea9f584204f3
mannju <manjunath.rs@unilogcorp.com>
2018-04-14 00:18:17 +0530

    Featured Products
    Implimented Featured Products on home page

--- ecombase/src/Pages/Home.vue
+++ ecombase/src/Pages/Home.vue
@@ -1,22 +1,27 @@
 <template>
     <section>
         <app-slider />
+        <br />
+        <FurCategories />
+        <br />
     </section>
 </template>
 
 <script>
 
 import banner from '../components/banner';
-
+import FeaturedCategories from '../components/featuredcategories'
 import 'swiper/dist/css/swiper.css'
 
 export default {
   components: {
-    'app-slider': banner
+    'app-slider': banner,
+    'FurCategories': FeaturedCategories
   }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  
 </style>
+
