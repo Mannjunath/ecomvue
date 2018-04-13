80ecdcf4c228f085946ce88a9e8162fd8be69fbf
mannju <manjunath.rs@unilogcorp.com>
2018-04-13 15:50:18 +0530

    implimented json

--- ecombase/src/Layouts/header.vue
+++ ecombase/src/Layouts/header.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container">
-    <div class="logo">
-      <a href="http://"><img src="https://www.unilogcorp.com/wp-content/uploads/2015/09/Unilog-logo.png" alt=""></a>
+  <div class="header">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="logo">
+          <a href="http://"><img src="https://www.unilogcorp.com/wp-content/uploads/2015/09/Unilog-logo.png" alt=""></a>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <nav>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="javascript:void(0)">Register</a></li>
+            <li><a href="javascript:void(0)">Login</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </div>
 </template>
@@ -15,5 +28,7 @@ export default {
 </script> 
 
 <style scoped>
-
+  .header{
+    padding: 20px 0px;
+  }
 </style>
--- ecombase/src/Layouts/main_nav.vue
+++ ecombase/src/Layouts/main_nav.vue
@@ -13,6 +13,12 @@
     
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
             <ul class="nav navbar-nav navbar-right">
                 <li><router-link to="./">Home</router-link></li>
                 <li><router-link to="./Categories">Categories</router-link></li>
