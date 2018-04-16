14ab458aa2a1239689abf29e9756e0876399c77a
Bharath <bharath.ks@unilogcorp.com>
2018-04-16 12:03:01 +0530

    push

---
+++ ecombase/ecombase/ecombase/src/Pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <app-slider />
+        <br />
+        <FurCategories />
+        <br />
+    </section>
+</template>
+
+<script>
+
+import banner from '../components/banner';
+import FeaturedCategories from '../components/featuredcategories'
+import 'swiper/dist/css/swiper.css'
+
+export default {
+  components: {
+    'app-slider': banner,
+    'FurCategories': FeaturedCategories
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
+
